eeb60027732e2c377429d8f061985335fca7b7c5
rufatsoltanli <146984697+rufatsoltanli@users.noreply.github.com>
2023-10-24 18:15:42 +0300

    Add files via upload

---
+++ New folder (2)/assets/js/script.js
@@ -0,0 +1,42 @@
+// let sum = 0
+// let i=0
+// while (i<100) {
+//     i++
+//     if (i % 2 === 0) {
+//         sum = [i]
+
+
+//     }
+
+// }
+// console.log(sum);
+// let a = 5
+// let b = 4
+// a = a + b
+// b = a - b
+// a = a - b
+// console.log(a);
+// console.log(b);
+
+// const arr = [3, 4, 7, 8, 10, 15, 18, 19, 23, 24]
+// for (let i = arr[0]; i <= arr[9]; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i   );
+
+//     }
+// }
+let sum = 0
+let answer
+let array= [2,4,6,8,10,12]
+for (let i = 0; i <= array.length; i+=2) {
+
+
+console.log(array[i]);
+    
+}
+
+// sum +=i
+    
+// console.log(sum);
+// answer=sum /array.length
+// console.log(answer);
